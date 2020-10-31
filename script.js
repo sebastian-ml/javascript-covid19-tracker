0b42fc03dcfc19126cf8540e9cc10c64a979a3a4
sebastian-ml <sebastian2549@wp.pl>
2020-10-31 13:58:10 +0100

    Add js code to gather all available countries codes

---
+++ script.js
@@ -0,0 +1,9 @@
+const url = 'https://corona-api.com/countries';
+let countryArray;
+const countriesCode = [];
+
+// Gather all countries code and store in an array
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonResp) => countryArray = jsonResp.data)
+    .then(() => countryArray.forEach((item) => countriesCode.push(item.code)))

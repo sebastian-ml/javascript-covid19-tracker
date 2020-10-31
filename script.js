6bfae8c926148f7c9c26f77c220837f7d16b3789
sebastian-ml <sebastian2549@wp.pl>
2020-10-31 15:43:54 +0100

    Change site color, add piechart for covid cases
    Data in piechart is currently fake, site color is darker

--- script.js
+++ script.js
@@ -7,3 +7,37 @@ fetch(url)
     .then((response) => response.json())
     .then((jsonResp) => countryArray = jsonResp.data)
     .then(() => countryArray.forEach((item) => countriesCode.push(item.code)))
+
+
+google.charts.load("current", {packages:["corechart"]});
+google.charts.setOnLoadCallback(drawChart);
+function drawChart() {
+    const data = google.visualization.arrayToDataTable([
+        ['Status', 'Cases'],
+        ['Active', 20],
+        ['Recovered', 10],
+        ['Deaths', 5],
+    ]);
+
+    const options = {
+        // title: 'My Daily Activities',
+        // pieHole: 0.3,
+        legend: 'none',
+        chartArea: {
+            left: 0,
+            top: 0,
+            width: "100%",
+            height: "100%",
+        },
+        colors: ['#f7cc20', '#8fb447', '#bf3c39'],
+        backgroundColor: { fill:'transparent' },
+        fontSize: 15,
+        is3D: true,
+        pieSliceTextStyle: {
+            color: 'black'
+        }
+    };
+
+    const chart = new google.visualization.PieChart(document.getElementById('donut-chart'));
+    chart.draw(data, options);
+}

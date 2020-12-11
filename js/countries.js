1e0f13c1175d77224d799ed00748dade1c96787f
sebastian-ml <sebastian2549@wp.pl>
2020-12-11 21:46:52 +0100

    Create function to get country code and store in localstorage

--- js/countries.js
+++ js/countries.js
@@ -1,10 +1,28 @@
 const url = 'https://cors-anywhere.herokuapp.com/https://corona-api.com/countries';
-let countryArray;
-const countriesCode = [];
 
-// Gather all countries code and store in an array
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonResp) => countryArray = jsonResp.data)
-    .then(() => countryArray.forEach((item) => countriesCode.push(item.code)))
+function checkForCountryUpdate() {
+    let timeSinceLastUpdate;
+    const countriesCode = JSON.parse(localStorage.getItem('countriesCode'));
+    if (countriesCode) timeSinceLastUpdate = new Date()
+        - new Date(countriesCode['last_update']);
 
+    if (!countriesCode || (timeSinceLastUpdate >= 1000 * 60 * 60 * 24)) {
+        let countryArray;
+        const countriesCode = [];
+
+        // Gather all countries code and store in an array
+        fetch(url)
+            .then((response) => response.json())
+            .then((jsonResp) => {
+                countryArray = jsonResp.data
+                countryArray.forEach((item) => countriesCode.push(item.code))
+                const data = {
+                    last_update: new Date(),
+                    code: countriesCode
+                }
+                localStorage.setItem('countriesCode', JSON.stringify(data));
+            })
+    }
+}
+
+checkForCountryUpdate();

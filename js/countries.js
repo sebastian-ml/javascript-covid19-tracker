e06d58ef14510021c35d1ca41182027e0537cdf1
sebastian-ml <sebastian2549@wp.pl>
2020-12-13 17:03:59 +0100

    Split checking country codes function into two functions
    One fetch function and one update

--- js/countries.js
+++ js/countries.js
@@ -1,28 +1,40 @@
 const url = 'https://cors-anywhere.herokuapp.com/https://corona-api.com/countries';
 
-function checkForCountryUpdate() {
-    let timeSinceLastUpdate;
-    const countriesCode = JSON.parse(localStorage.getItem('countriesCode'));
-    if (countriesCode) timeSinceLastUpdate = new Date()
-        - new Date(countriesCode['last_update']);
+// Gather all countries code from the given url and store in an array
+function fetchCountriesCode(url) {
+    return fetch(url)
+        .then((response) => response.json())
+        .then((jsonResp) => {
+            const countriesCode = [];
+            (jsonResp.data).forEach((item) => countriesCode.push(item.code))
 
-    if (!countriesCode || (timeSinceLastUpdate >= 1000 * 60 * 60 * 24)) {
-        let countryArray;
-        const countriesCode = [];
+            return countriesCode;
+        })
+}
 
-        // Gather all countries code and store in an array
-        fetch(url)
-            .then((response) => response.json())
-            .then((jsonResp) => {
-                countryArray = jsonResp.data
-                countryArray.forEach((item) => countriesCode.push(item.code))
-                const data = {
-                    last_update: new Date(),
-                    code: countriesCode
-                }
-                localStorage.setItem('countriesCode', JSON.stringify(data));
-            })
+// Check if countries codes in local storage should be updated
+function checkForCountryUpdate(url) {
+    if(typeof(Storage) !== void(0)) {
+        let timeSinceLastUpdate;
+        const countriesCode = JSON.parse(localStorage.getItem('countriesCode'));
+
+        if (countriesCode) timeSinceLastUpdate = new Date()
+            - new Date(countriesCode['last_update']);
+
+        if (!countriesCode || (timeSinceLastUpdate >= 1000 * 60 * 60 * 24)) {
+            const countriesCode = fetchCountriesCode(url);
+
+            // Save countries code in localStorage
+            countriesCode
+                .then(countriesCodeArray => {
+                    const data = {
+                        last_update: new Date(),
+                        code: countriesCodeArray
+                    }
+                    localStorage.setItem('countriesCode', JSON.stringify(data));
+                })
+        }
     }
 }
 
-checkForCountryUpdate();
+checkForCountryUpdate(url);
